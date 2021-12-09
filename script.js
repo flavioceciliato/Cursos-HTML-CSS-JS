575e787f3e49055d837b93768cb6d224dc5c619b
Flávio Ceciliato <flavio_2000jbt@hotmail.com>
2021-12-09 17:09:30 -0300

    att

---
+++ script.js
@@ -0,0 +1,57 @@
+/*var a, b, sum;
+a = 4;
+b = 4;
+var eNum;*/
+
+// CONDICIONAL
+/* if (alguma condição) {
+    faça algo <- Bloco de Código
+} else {
+    Faça outra coisa
+}
+
+// OPERADOR TERNARIO
+a == b ? faça algo se for true : faça algo se for falso;
+
+if (a == b){
+    eNum = true;
+} else{
+    eNum = false;
+}
+alert("São Iguais? " + eNum);*/
+
+// ARRAYS
+/*var canetas;
+canetas = ["Vermelho", "Azul", "Verde"];
+//var variosElementos = ["Chaves", 5, false, asdasd]
+alert(canetas[2]);
+canetas[2] = "VerdeClaro";
+alert(canetas[2]);*/
+
+// FUNÇÃO
+/*function multiplicarTermos(a,b){
+    var resultado = a * b;
+    return resultado;
+}
+alert(multiplicarTermos(a,b));*/
+
+// OBJETOS
+/*var curso = new Object();
+var curso = {
+    titulo: "JavaScript",
+    instrutora: "Amanda Cavallaro",
+    attTitulo: function(){
+        return curso.titulo = "JavaScript do Linkedin Learning";
+    }
+}
+alert(curso.titulo);
+alert(curso.attTitulo());*/
+
+//CONSTRUTORES
+/*function Curso(titulo, instrutora){
+    this.titulo = titulo,
+    this.instrutora = instrutora;
+}
+
+var curso01 = new Curso("JavaScript", "Amanda Cavallaro");*/
+//OU PODEMOS ANINHAR OBJETOS COM var cursos = [new curso(...),new curso2(...)]
